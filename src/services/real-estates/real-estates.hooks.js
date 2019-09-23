f7e433488d93d419c51c7d5985ea425c7540d8aa
Yordan Galabov <galabovj@gmail.com>
2019-09-23 17:05:25 +0300

    check if record is already in the database

--- src/services/real-estates/real-estates.hooks.js
+++ src/services/real-estates/real-estates.hooks.js
@@ -1,33 +1,52 @@
+const { Forbidden } = require('@feathersjs/errors');
 
+const isIdUnique = async (realEstatesTable, real_estates_id) => {
+    const token = await realEstatesTable.findOne({
+        where: { real_estates_id },
+    });
+    return token !== null ? false : true;
+};
 
 module.exports = {
-  before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    before: {
+        all: [],
+        find: [],
+        get: [],
+        create: [
+            // check if id is already in the database
+            async context => {
+                const sequelize = context.app.get('sequelizeClient');
+                const { real_estates } = sequelize.models;
+                const { real_estates_id } = context.data;
+                if (!(await isIdUnique(real_estates, real_estates_id))) {
+                    throw new Forbidden(
+                        'This real estate is already in the database'
+                    );
+                }
+            },
+        ],
+        update: [],
+        patch: [],
+        remove: [],
+    },
 
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    after: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: [],
+    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: [],
+    },
 };
