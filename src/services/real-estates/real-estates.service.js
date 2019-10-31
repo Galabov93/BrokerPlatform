4cdb4661eee37ec9e9b2e25ff0523314fb8680d9
Yordan Galabov <galabovj@gmail.com>
2019-10-31 08:28:40 +0200

    prettier format

--- src/services/real-estates/real-estates.service.js
+++ src/services/real-estates/real-estates.service.js
@@ -3,20 +3,20 @@ const createService = require('feathers-sequelize');
 const createModel = require('../../models/real-estates.model');
 const hooks = require('./real-estates.hooks');
 
-module.exports = function (app) {
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+module.exports = function(app) {
+    const Model = createModel(app);
+    const paginate = app.get('paginate');
 
-  const options = {
-    Model,
-    paginate
-  };
+    const options = {
+        Model,
+        paginate,
+    };
 
-  // Initialize our service with any options it requires
-  app.use('/real-estates', createService(options));
+    // Initialize our service with any options it requires
+    app.use('/real-estates', createService(options));
 
-  // Get our initialized service so that we can register hooks
-  const service = app.service('real-estates');
+    // Get our initialized service so that we can register hooks
+    const service = app.service('real-estates');
 
-  service.hooks(hooks);
+    service.hooks(hooks);
 };
