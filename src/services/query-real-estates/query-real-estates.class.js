075a75d537febcca131e15629d3522ab6f1670d3
Yordan Galabov <galabovj@gmail.com>
2019-11-02 19:36:33 +0200

    zdrasti mana

--- src/services/query-real-estates/query-real-estates.class.js
+++ src/services/query-real-estates/query-real-estates.class.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-unused-vars */
 const Sequelize = require('sequelize');
 
+function formatDataFromReactSelect(dataArray) {
+    return dataArray.map(object => JSON.parse(object).value);
+}
+
 class Service {
     constructor(options) {
         this.options = options || {};
@@ -8,20 +12,74 @@ class Service {
     }
 
     async find(params) {
+        // constructionType,
+        // neighbourhoods,
+        // priceFrom,
+        // priceTo,
+        // sizeFrom,
+        // sizeTo
+
+        const { query } = params;
         const sequelize = this.app.get('sequelizeClient');
         const { real_estates } = sequelize.models;
         const Op = Sequelize.Op;
 
+        function getRealEstatesSellType(query) {
+            if (!query.propertySellType) {
+                return {};
+            } else {
+                return {
+                    real_estates_sell_type: {
+                        [Op.eq]: query.propertySellType,
+                    },
+                };
+            }
+        }
+
+        function getFiltersWhereIn(query, propertyName, databaseColumnName) {
+            if (!query[propertyName]) {
+                return {};
+            } else {
+                return {
+                    databaseColumnName: {
+                        [Op.in]: formatDataFromReactSelect(query[propertyName]),
+                    },
+                };
+            }
+        }
+
+        function getFiltersWhereBetween(
+            query,
+            propertyName,
+            databaseColumnName
+        ) {
+            const fromValue = query[`from${propertyName}`] || 0;
+            const toValue = query[`to${propertyName}`] || 0;
+            if (!toValue) {
+                return {};
+            } else {
+            }
+        }
+
+        const sellTypeFilter = getRealEstatesSellType(query);
+        const neighbourhoodsFilter = getFiltersWhereIn(
+            query,
+            'neighbourhoods',
+            'real_estates_neighborhood'
+        );
+        const constructionTypeFilter = getFiltersWhereIn(
+            query,
+            'constructionType',
+            'real_estates_construction_type'
+        );
+
         const filteredData = await real_estates.findAll({
             where: {
-                real_estates_sell_type: {
-                    [Op.eq]: 'rent',
-                },
+                ...sellTypeFilter,
+                ...constructionTypeFilter,
+                ...neighbourhoodsFilter,
                 real_estates_price_in_euro: {
                     [Op.between]: [0, 500],
-                },
-                real_estates_neighborhood: {
-                    [Op.in]: ['Младост 2'],
                 },
                 real_estates_size: {
                     [Op.between]: [0, 100],
