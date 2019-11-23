d45073dcbbe9719937ab6554210c646a0511c797
Yordan Galabov <galabovj@gmail.com>
2019-11-23 22:05:57 +0200

    remove irrelevant files

--- test/services/query-real-estates.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'query-real-estates\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('query-real-estates');
-    expect(service).toBeTruthy();
-  });
-});
--- test/services/real-estates.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'real-estates\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('real-estates');
-    expect(service).toBeTruthy();
-  });
-});
