f18e06c394ce6f24255a9ba444d9a7c07d99d096
Yordan Galabov <galabovj@gmail.com>
2019-11-26 00:29:25 +0200

    add services

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,8 @@ const users = require('./users/users.service.js');
 const queryRealEstates = require('./query-real-estates/query-real-estates.service.js');
 const getPropertyLinks = require('./get-property-links/get-property-links.service.js');
 const scrapeLinksService = require('./scrape-links-service/scrape-links-service.service.js');
+const uploadImageToCloud = require('./upload-image-to-cloud/upload-image-to-cloud.service.js');
+const scrapeImagesFromUrl = require('./scrape-images-from-url/scrape-images-from-url.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
     app.configure(realEstates);
@@ -10,4 +12,6 @@ module.exports = function(app) {
     app.configure(queryRealEstates);
     app.configure(getPropertyLinks);
     app.configure(scrapeLinksService);
+    app.configure(uploadImageToCloud);
+    app.configure(scrapeImagesFromUrl);
 };
