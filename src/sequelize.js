e763bd60d047a8324227019b2cd3bf6cab0ad013
Yordan Galabov <galabovj@gmail.com>
2020-07-15 16:35:00 +0300

    add relationship hooks

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize');
 
-module.exports = function(app) {
+module.exports = function (app) {
     const connectionString = process.env.DB_CONNECTION_STRING;
     const sequelize = new Sequelize(connectionString, {
         dialect: 'mysql',
-        logging: false,
         define: {
             freezeTableName: true,
         },
@@ -13,12 +12,12 @@ module.exports = function(app) {
 
     app.set('sequelizeClient', sequelize);
 
-    app.setup = function(...args) {
+    app.setup = function (...args) {
         const result = oldSetup.apply(this, args);
 
         // Set up data relationships
         const models = sequelize.models;
-        Object.keys(models).forEach(name => {
+        Object.keys(models).forEach((name) => {
             if ('associate' in models[name]) {
                 models[name].associate(models);
             }
--- src/services/notes/notes.hooks.js
+++ src/services/notes/notes.hooks.js
@@ -1,33 +1,67 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
 module.exports = {
-  before: {
-    all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+    before: {
+        all: [authenticate('jwt')],
+        find: [
+            function (context) {
+                const usersModel = context.app.services.users.Model;
+                const realEstatesModel =
+                    context.app.services['real-estates'].Model;
 
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
+                context.params.sequelize = {
+                    attributes: ['id', 'text', 'createdAt'],
+                    through: {
+                        attributes: [],
+                    },
 
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
+                    include: [
+                        {
+                            model: usersModel,
+                            attributes: ['email', 'id'],
+                        },
+                        {
+                            model: realEstatesModel,
+                            attributes: ['real_estates_id', 'id'],
+                        },
+                    ],
+                };
+
+                return context;
+            },
+        ],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: [],
+    },
+
+    after: {
+        all: [],
+        find: [],
+        get: [],
+        create: [
+            function (context) {
+                const email = context.params.user.email;
+
+                context.result['user.email'] = email;
+
+                return context;
+            },
+        ],
+        update: [],
+        patch: [],
+        remove: [],
+    },
+
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: [],
+    },
 };
